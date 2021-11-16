8824500ebc912ea7f21b4a8863278ea3ed0e80da
sc0der <siriusproger@gmail.com>
2021-11-16 22:35:06 +0500

    created stores_product table

--- create_table.sql
+++ create_table.sql
@@ -14,10 +14,18 @@ CREATE TABLE IF NOT EXISTS products(
     id serial primary key,
     name varchar not null,
     expiration date not null,
-    vendor_id integer, foreign key(vendor_id) references vendors(id)
+    vendor_id integer, 
+    foreign key(vendor_id) references vendors(id)
 );
 
 CREATE table if not EXISTS stores(
     id serial primary key,
     name varchar not null,
 );
+
+create table if not EXISTS stores_products(
+    store_id integer NOT NULL,
+    product_id integer NOT NULL,
+    FOREIGN KEY(store_id) REFERENCES stores(id),
+    FOREIGN KEY(product_id) REFERENCES products(id),
+)
